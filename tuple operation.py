ae17434a41c097e59c75c52567aa119139a1b869
Tahsin Tabassum <tabassumrifa03@gmail.com>
2024-09-21 17:03:24 +0600

    first commit

---
+++ tuple operation.py
@@ -0,0 +1,10 @@
+tuplex = ("tuple", True, 4, 2.5)
+print(tuplex)
+
+tuplex = tuplex + ("Tiktiki",)
+print(tuplex)
+
+print(   tuplex[4]    )
+
+tuple1 = (30, 50, 60, 50, 10, 50, 50)
+print(   tuple1.count(50)  )
